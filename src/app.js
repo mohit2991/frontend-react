6cfa06eba7e56e8606f5a13d8d85155a603d6e0e
Mohit Chauhan <mohit2991kumar@gmail.com>
2024-05-14 22:31:44 +0530

    Counter - State and Props

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/app.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Counter from "./components/Counter";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
+      <>
         <div>
-          <Home />
+          <Header />
+          <div>
+            <Home />
+            <Counter value={100} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </>
     );
   }
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import CounterPrint from "./CounterPrint";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Counter = (Props) => {
+  const { value } = Props;
+  // State/Props- Hook
+  const [number, setNumber] = useState(value);
+  const [array, setArray] = useState([]);
+
+  const increment = () => {
+    let arrayValue = array;
+    arrayValue.push(number + 1);
+
+    setArray(arrayValue);
+    setNumber(number + 1);
+  };
+
+  const decrement = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <CounterPrint number={number} />
+      <button onClick={increment} className="mt-4">Incement</button>
+      <button onClick={decrement}>Decrement</button>
+      <div>
+        {array.map((elemet) => {
+          return <div>{elemet}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
